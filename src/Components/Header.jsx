e1a7bbf815e11666d2280991bffc1551d16a1120
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-01-24 19:53:56 +0600

    custom header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,14 +9,14 @@ const Header = async () => {
     const isUserAuthenticated = await isAuthenticated()
     console.log(isUserAuthenticated)
     return (
-        <div className="navbar w-full bg-slate-100">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Blogger</a>
+        <div className="bg-slate-100 px-5 py-3 flex items-center justify-between">
+            <div className="">
+                <a className="text-xl">Blogger</a>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/profile'>Profile</Link></li>
+            <div className="flex gap-3 items-center">
+                <ul className="px-1 flex gap-2">
+                    <li className='btn'><Link href='/'>Home</Link></li>
+                    <li className='btn'><Link href='/profile'>Profile</Link></li>
                 </ul>
                 <div className='flex gap-2'>
                     {

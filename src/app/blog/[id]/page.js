89887adcc161bb4714bb08087f0376f02d77cc22
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-01-24 12:49:49 +0600

    added blog details page

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,1 +1,18 @@
 
+const BlogDetails = async ({ params }) => {
+    const { id } = params
+    console.log(id)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const post = await res.json()
+    console.log(post)
+    return (
+        <div className="card bg-white mt-5 border rounded-sm shadow-sm w-10/12 mx-auto">
+            <div className="card-body">
+                <h2 className="card-title">{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BlogDetails;

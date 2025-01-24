8addd43c359644aab381d45d37a64feb9a86edef
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-01-24 12:50:31 +0600

    added my profile page

---
+++ src/app/profile/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='w-11/12 mx-auto mt-3'>
+            <h1 className='text-4xl font-semibold'>Welcome to your profile?</h1>
+        </div>
+    );
+};
+
+export default page;
